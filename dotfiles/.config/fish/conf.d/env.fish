18a2ad8386b8487dacabc128e1baabd7a607c39e
Tamara <tamara.zoe.schmitz@posteo.de>
2022-06-13 21:14:49 +0200

    fish: remove old environmentals

--- dotfiles/.config/fish/conf.d/env.fish
+++ dotfiles/.config/fish/conf.d/env.fish
@@ -6,9 +6,4 @@ if test $XDG_SESSION_TYPE = "wayland"
     export QT_QPA_PLATFORM=wayland
 end
 
-export cheat () { curl cht.sh/$1 }
-
 # Devel environment exports
-#export JAVA_HOME=/usr/lib64/jvm/java-1.8.0-openjdk
-export PATH="$PATH:$HOME/learning/flutter/flutter"
-export PATH="$PATH:/usr/lib64/ruby/gems/2.7.0/gems"
