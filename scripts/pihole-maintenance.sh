491ca497efeb0ea13550141b52c9d070ebfa21d7
Tamara Schmitz <tamara.zoe.schmitz@posteo.de>
2022-07-10 19:55:16 +0200

    add comment to pihole maintenance script

--- scripts/pihole-maintenance.sh
+++ scripts/pihole-maintenance.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# run by a crontab:
+# 0 5  *   *   1,5      date -Iseconds >> /var/log/maintenance.log && /root/maintenance.sh >> /var/log/maintenance.log
+
 set -x
 ionice -c idle nice -n19 apt update
 ionice -c idle nice -n19 apt -y full-upgrade
